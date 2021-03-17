be0edf3ca63b4c721665a1525b9f7c682f6cfce6
Razvan Predescu <rzvdaniel@gmail.com>
2021-03-17 22:10:55 +0000

    No email confirmation for now

--- Automaton.Studio/Startup.cs
+++ Automaton.Studio/Startup.cs
@@ -34,7 +34,7 @@ namespace Automaton.Studio
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddRazorPages();
             services.AddServerSideBlazor();
