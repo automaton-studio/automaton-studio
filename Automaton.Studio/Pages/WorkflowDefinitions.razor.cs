35bcfeb65ed561800d577acd74f86868790a765a
Razvan Predescu <rzvdaniel@gmail.com>
2021-03-20 12:47:48 +0000

    Added Workflow Definition page

---
+++ Automaton.Studio/Pages/WorkflowDefinitions.razor.cs
@@ -0,0 +1,25 @@
+﻿using ElsaDashboard.Shared.Rpc;
+using Microsoft.AspNetCore.Components;
+using System.Threading.Tasks;
+
+namespace Automaton.Studio.Pages
+{
+    partial class WorkflowDefinitions
+    {
+        private int currentCount = 0;
+
+        [Inject] private IWorkflowDefinitionService WorkflowDefinitionService { get; set; } = default!;
+        [Inject] private IActivityService ActivityService { get; set; } = default!;
+
+        protected override async Task OnInitializedAsync()
+        {
+            var workflowDefinitions = await WorkflowDefinitionService.ListAsync();
+        }
+
+
+        private void IncrementCount()
+        {
+            currentCount++;
+        }
+    }
+}
