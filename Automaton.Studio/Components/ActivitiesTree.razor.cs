d4910efcc11275c4cf28a43da043fc223282beac
Razvan Predescu <rzvdaniel@gmail.com>
2021-06-20 00:49:35 +0100

    * Updated designer layout * Introduced Search Activities input

--- Automaton.Studio/Components/ActivitiesTree.razor.cs
+++ Automaton.Studio/Components/ActivitiesTree.razor.cs
@@ -1,18 +1,35 @@
 ﻿using Automaton.Studio.ViewModels;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
 using System.Threading.Tasks;
 
 namespace Automaton.Studio.Components
 {
     partial class ActivitiesTree : ComponentBase
     {
-        [Inject] private ITreeActivityViewModel TreeActivityViewModel { get; set; } = default!;
+        [Inject] 
+        private ITreeActivityViewModel TreeActivityViewModel { get; set; } = default!;
+
+        private string searchText { get; set; }
 
         protected override async Task OnInitializedAsync()
         {
             TreeActivityViewModel.Initialize();
 
             await base.OnInitializedAsync();
+        }
+
+        private void Handle(string value)
+        {
+        }
+
+        public async Task OnSearch()
+        {
+            //searchText
+        }
+
+        private async Task OnEnter(KeyboardEventArgs e)
+        {
         }
     }
 }
