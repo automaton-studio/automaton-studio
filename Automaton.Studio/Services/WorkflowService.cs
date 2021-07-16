7dafd7cd00431b759eb4511ed5303c175d1716b8
Razvan Predescu <rzvdaniel@gmail.com>
2021-07-16 22:07:35 +0100

    Added a WorkflowService to be able to run workflows from server side in development mode

---
+++ Automaton.Studio/Services/WorkflowService.cs
@@ -0,0 +1,35 @@
+﻿using Elsa.Persistence;
+using Elsa.Services;
+using System.Threading.Tasks;
+
+namespace Automaton.Studio.Services
+{
+    public class WorkflowService : IWorkflowService
+    {
+        private readonly IWorkflowDefinitionStore workflowDefinitionStore;
+        private readonly IWorkflowBlueprintMaterializer workflowBlueprintMaterializer;
+        private readonly IStartsWorkflow startsWorkflow;
+
+        public WorkflowService(
+            IWorkflowBlueprintMaterializer workflowBlueprintMaterializer,
+            IWorkflowDefinitionStore workflowDefinitionStore,
+            IStartsWorkflow startsWorkflow)
+        {
+            this.workflowDefinitionStore = workflowDefinitionStore;
+            this.workflowBlueprintMaterializer = workflowBlueprintMaterializer;
+            this.startsWorkflow = startsWorkflow;
+        }
+
+        public async Task RunWorkflow(string workflowId)
+        {
+            // Retrieve workflow definition from store.
+            var workflowDefinition = await workflowDefinitionStore.FindAsync(new Elsa.Persistence.Specifications.WorkflowDefinitions.WorkflowDefinitionIdSpecification(workflowId));
+
+            // Create blueprint from wokflow definition.
+            var workflowBlueprint = await workflowBlueprintMaterializer.CreateWorkflowBlueprintAsync(workflowDefinition);
+
+            // Start workflow.
+            var workflowInstance = await startsWorkflow.StartWorkflowAsync(workflowBlueprint);
+        }
+    }
+}
