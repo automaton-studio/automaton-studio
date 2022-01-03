407e905f24643797b96bdb1c4a2614d1d8df85a4
Razvan Predescu <rzvdaniel@gmail.com>
2022-01-03 23:40:59 +0000

    Removed unnecessary ViewModelLocator

--- src/Automaton.Studio.Desktop/MainWindow.xaml.cs
+++ src/Automaton.Studio.Desktop/MainWindow.xaml.cs
@@ -1,9 +1,4 @@
-// Copyright (c) .NET Foundation. All rights reserved.
-// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
-
-using Automaton.Studio.ViewModels;
 using Microsoft.Extensions.DependencyInjection;
-using System.Reflection;
 using System.Windows;
 
 namespace Automaton.Studio.Desktop
