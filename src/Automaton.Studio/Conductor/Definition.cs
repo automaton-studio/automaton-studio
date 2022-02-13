d4df5659608299b6da6125ebecdf7961f089ba77
Razvan Predescu <rzvdaniel@gmail.com>
2022-02-13 17:45:18 +0000

    Removed unnecessary error handling classes

--- src/Automaton.Studio/Conductor/Definition.cs
+++ src/Automaton.Studio/Conductor/Definition.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Automaton.Core.Enums;
+using System;
 using System.Collections.Generic;
 
 namespace Automaton.Studio.Conductor
--- src/Automaton.Studio/Conductor/Step.cs
+++ src/Automaton.Studio/Conductor/Step.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Automaton.Core.Enums;
+using System;
 using System.Collections.Generic;
 using System.Dynamic;
 
