af36722cd2ec25bb5e7c664e911617f4b6b36fb5
Razvan Predescu <rzvdaniel@gmail.com>
2022-03-12 23:48:06 +0000

    Revert "Removed unnecessary AddVariable step"
    This reverts commit 65d658dacd99414774fd84a78ddd82c7a0ec582c.

---
+++ src/Automaton.Studio/Steps/AddVariable/AddVariableValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace Automaton.Studio.Steps.AddVariable
+{
+    public class AddVariableValidator : AbstractValidator<AddVariableStep>
+    {
+        public AddVariableValidator()
+        {
+            RuleFor(x => x.VariableName).NotEmpty().MaximumLength(256).WithMessage("Name required");
+            RuleFor(x => x.VariableValue).NotNull().WithMessage("Variable required");
+        }
+    }
+}
