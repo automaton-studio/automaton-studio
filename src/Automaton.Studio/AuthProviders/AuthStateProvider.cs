f9ec75f9c8437a5ab0a2b00b019b8fc002a9998f
rzvdaniel <rzvdaniel@gmail.com>
2022-04-17 14:02:42 +0100

    Beautify hardcoded strings

--- src/Automaton.Studio/AuthProviders/AuthStateProvider.cs
+++ src/Automaton.Studio/AuthProviders/AuthStateProvider.cs
@@ -12,6 +12,10 @@ namespace Automaton.Studio.AuthProviders
 {
     public class AuthStateProvider : AuthenticationStateProvider
     {
+        private const string Bearer = "bearer";
+        private const string ClaimExp = "exp";
+        private const string ClaimJwtAuthType = "jwtAuthType";
+
         private readonly HttpClient httpClient;
         private readonly ILocalStorageService localStorage;
         private readonly IRefreshTokenService refreshTokenService;
@@ -41,10 +45,10 @@ namespace Automaton.Studio.AuthProviders
                     return anonymous;
                 }
 
-                httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", authToken);
+                httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(Bearer, authToken);
 
                 var claims = JwtParser.ParseClaimsFromJwt(authToken);
-                var claimsIdentity = new ClaimsIdentity(claims, "jwtAuthType");
+                var claimsIdentity = new ClaimsIdentity(claims, ClaimJwtAuthType);
                 var claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
                 var authenticationState = new AuthenticationState(claimsPrincipal);
 
@@ -68,7 +72,7 @@ namespace Automaton.Studio.AuthProviders
 
         public void NotifyUserAuthentication(string token)
         {
-            var authenticatedUser = new ClaimsPrincipal(new ClaimsIdentity(JwtParser.ParseClaimsFromJwt(token), "jwtAuthType"));
+            var authenticatedUser = new ClaimsPrincipal(new ClaimsIdentity(JwtParser.ParseClaimsFromJwt(token), ClaimJwtAuthType));
             var authState = Task.FromResult(new AuthenticationState(authenticatedUser));
 
             NotifyAuthenticationStateChanged(authState);
@@ -84,9 +88,12 @@ namespace Automaton.Studio.AuthProviders
         private async Task<string> GetAuthToken()
         {
             var authToken = await localStorage.GetItemAsync<string>("authToken");
-            var authState = new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity(JwtParser.ParseClaimsFromJwt(authToken), "jwtAuthType")));
+            var claims = JwtParser.ParseClaimsFromJwt(authToken);
+            var claimsIdentity = new ClaimsIdentity(claims, ClaimJwtAuthType);
+            var claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
+            var authState = new AuthenticationState(claimsPrincipal);
             var user = authState.User;
-            var exp = user.FindFirst(c => c.Type.Equals("exp")).Value;
+            var exp = user.FindFirst(c => c.Type.Equals(ClaimExp)).Value;
             var expTime = DateTimeOffset.FromUnixTimeSeconds(Convert.ToInt64(exp));
             var timeUTC = DateTime.UtcNow;
             var diff = expTime - timeUTC;
