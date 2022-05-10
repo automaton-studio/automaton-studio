748feeec84be071830251b36926512183aff220e
rzvdaniel <rzvdaniel@gmail.com>
2022-05-10 23:12:14 +0100

    AppConfiguration in Studio no longer need some properties

--- src/Automaton.Studio/Config/AppConfiguration.cs
+++ src/Automaton.Studio/Config/AppConfiguration.cs
@@ -2,8 +2,6 @@
 {
     public class AppConfiguration
     {
-        public string WebApiUrl { get; set; }
-        public string FlowsUrl { get; set; }
         public bool IsDesktop { get; set; }
     }
 }
