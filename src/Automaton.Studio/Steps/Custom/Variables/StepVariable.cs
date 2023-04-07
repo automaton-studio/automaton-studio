b189c32d0af0c4116e6b7bfcfd283e9cc7172f29
rzvdaniel <rzvdaniel@gmail.com>
2023-04-07 16:56:43 +0200

    Render step variable components

---
+++ src/Automaton.Studio/Steps/Custom/Variables/StepVariable.cs
@@ -0,0 +1,10 @@
+﻿using Automaton.Core.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace Automaton.Studio.Steps.Custom.Variables
+{
+    public abstract class StepVariable : ComponentBase
+    {
+        [Parameter] public CustomStepVariable Variable { get; set; }  
+    }
+}
