dbb3bdcd3878a90451c8147754ea04fcb49ac329
rzvdaniel <rzvdaniel@gmail.com>
2023-06-11 21:08:43 +0200

    Remove unnecessary StepVariable constructors

--- src/Automaton.Core/Models/StepVariable.cs
+++ src/Automaton.Core/Models/StepVariable.cs
@@ -4,23 +4,9 @@ namespace Automaton.Core.Models;
 
 public class StepVariable
 {
+    public string Id { get; set; }
     public string? Name { get; set; }
     public VariableType Type { get; set; } = VariableType.String;
     public object? Value { get; set; }
     public string? Description { get; set; }
-
-    public StepVariable()
-    {
-    }
-
-    public StepVariable(string name)
-    {
-        Name = name;
-    }
-
-    public StepVariable(string name, object value)
-    {
-        Name = name;
-        Value = value;
-    }
 }
