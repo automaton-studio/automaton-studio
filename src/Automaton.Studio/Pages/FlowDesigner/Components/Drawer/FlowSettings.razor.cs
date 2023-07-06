8156610216715c63fb3338837ec9a0bd03ee3d29
rzvdaniel <rzvdaniel@gmail.com>
2023-07-07 00:22:25 +0200

    Updated flow settings laout

--- src/Automaton.Studio/Pages/FlowDesigner/Components/Drawer/FlowSettings.razor.cs
+++ src/Automaton.Studio/Pages/FlowDesigner/Components/Drawer/FlowSettings.razor.cs
@@ -1,7 +1,6 @@
 ﻿using Automaton.Studio.Domain;
 using Blazored.FluentValidation;
 using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Forms;
 using System.Threading.Tasks;
 
 namespace Automaton.Studio.Pages.FlowDesigner.Components.Drawer;
@@ -17,31 +16,5 @@ public partial class FlowSettings : ComponentBase
     protected override async Task OnInitializedAsync()
     {
         await base.OnInitializedAsync();
-    }
-
-    private async Task OnFinish(EditContext editContext)
-    {
-        try
-        {
-            loading = true;
-
-            if (fluentValidationValidator.Validate(options => options.IncludeAllRuleSets()))
-            {
-
-            }
-        }
-        catch (Exception ex)
-        {
-           // await MessageService.Error("User profile update failed");
-        }
-        finally
-        {
-            loading = false;
-        }
-    }
-
-    private void OnFinishFailed(EditContext editContext)
-    {
-        // Do nothing if form validation fails
     }
 }
