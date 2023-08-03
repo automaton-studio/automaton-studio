f9bb6e18f7d522ab88e3b8cdf711e72114b2eb02
rzvdaniel <rzvdaniel@gmail.com>
2023-08-03 23:47:33 +0200

    Revert "One layout only"
    This reverts commit c7ffefe7b6e9ed176763d295486345c46ed946b9.

---
+++ src/Automaton.Runner/Shared/SetupLayout.razor.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Automaton.Runner.Shared
+{
+    public partial class SetupLayout : LayoutComponentBase
+    {
+        private bool MenuCollapsed { get; set; }
+
+        [Inject] NavigationManager NavigationManager { get; set; } = default!;
+
+        void ToggleCollapsed()
+        {
+            MenuCollapsed = !MenuCollapsed;
+        }
+    }
+}
