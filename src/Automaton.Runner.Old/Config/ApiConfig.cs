17c9c57a6a77e514726e75e33f53e2c77376ee37
rzvdaniel <rzvdaniel@gmail.com>
2023-08-17 21:41:14 +0200

    Removed old runner

--- src/Automaton.Runner.Old/Config/ApiConfig.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Automaton.Runner.Core.Config;
-
-public class ApiConfig
-{
-    public string BaseUrl { get; set; }
-    public string WorkflowHubUrl { get; set; }
-    public string RegistrationUrl { get; set; }
-    public string FlowsUrl { get; set; }
-}
--- src/Automaton.Runner.Old/Config/AppConfig.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Automaton.Runner.Core.Config;
-
-public class AppConfig
-{
-    public string RunnerName { get; set; }
-
-    public bool IsRunnerRegistered() => !string.IsNullOrEmpty(RunnerName);
-}
