1dd5f1652fa88e7fba87e1fab5f50a7eb88fd7d7
rzvdaniel <rzvdaniel@gmail.com>
2023-09-09 18:31:22 +0200

    Updated main menu

---
+++ src/Automaton.Studio/Shared/FlowMenu.razor.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Automaton.Studio.Shared
+{
+    public partial class FlowMenu : ComponentBase
+    {
+        [Parameter] public bool Collapsed { get; set; }
+    }
+}
---
+++ src/Automaton.Studio/Shared/NavMenu.razor.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Automaton.Studio.Shared
+{
+    public partial class NavMenu : ComponentBase
+    {
+        private bool collapsed;
+
+        [Parameter] public bool Collapsed 
+        { 
+            get 
+            {  
+                return collapsed; 
+            } 
+            set
+            {
+                collapsed = value;
+                MenuParameters = GetMenuParameters();
+            }
+        }
+
+        public Type Menu { get; set; } = typeof(MainMenu);
+
+        public Dictionary<string, object> MenuParameters { get; set; }
+
+        public NavMenu()
+        {
+            MenuParameters = GetMenuParameters();
+        }
+
+        private Dictionary<string,object> GetMenuParameters()
+        {
+            return new Dictionary<string, object>()
+            {
+                { "Collapsed", Collapsed }
+            };
+        }
+    }
+}
