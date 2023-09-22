067e553f0a39b1cfbf8479daa345313f02d8c508
rzvdaniel <rzvdaniel@gmail.com>
2023-09-23 00:19:01 +0200

    Removed migrations for simplicity

---
+++ src/Automaton.Studio.Server.MySql.Migrations/20230922220701_Logs.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace Automaton.Studio.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class Logs : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Runners",
+                type: "longtext",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreatedAt",
+                table: "RefreshTokens",
+                type: "datetime(6)",
+                nullable: false,
+                defaultValue: new DateTime(2023, 9, 23, 0, 7, 0, 932, DateTimeKind.Local).AddTicks(1737),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime(6)",
+                oldDefaultValue: new DateTime(2023, 8, 24, 23, 33, 43, 528, DateTimeKind.Local).AddTicks(2381));
+
+            migrationBuilder.CreateTable(
+                name: "Logs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    Level = table.Column<string>(type: "longtext", nullable: false),
+                    Message = table.Column<string>(type: "longtext", nullable: false),
+                    MessageTemplate = table.Column<string>(type: "longtext", nullable: false),
+                    Exception = table.Column<string>(type: "longtext", nullable: true),
+                    Properties = table.Column<string>(type: "longtext", nullable: false),
+                    Timestamp = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    EventType = table.Column<string>(type: "longtext", nullable: true),
+                    UserName = table.Column<string>(type: "longtext", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Logs", x => x.Id);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Logs");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Runners",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreatedAt",
+                table: "RefreshTokens",
+                type: "datetime(6)",
+                nullable: false,
+                defaultValue: new DateTime(2023, 8, 24, 23, 33, 43, 528, DateTimeKind.Local).AddTicks(2381),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime(6)",
+                oldDefaultValue: new DateTime(2023, 9, 23, 0, 7, 0, 932, DateTimeKind.Local).AddTicks(1737));
+        }
+    }
+}
--- src/Automaton.Studio.Server/README.md
+++ src/Automaton.Studio.Server/README.md
@@ -2,71 +2,12 @@
 
 ## Prerequisites to build and run solution
 
-
 ### Entity Framework CLI installation
 
 The dotnet ef tool is no longer part of the .NET Core SDK
 This change allows us to ship dotnet ef as a regular .NET CLI tool that can be installed as either a global or local tool. For example, to be able to manage migrations or scaffold a DbContext, install dotnet ef as a global tool typing the following command:
 
 ``dotnet tool install --global dotnet-ef``
-
-## Database Providers Setup
-
-### Handle multiple providers migration
-https://learn.microsoft.com/en-us/ef/core/managing-schemas/migrations/projects?tabs=dotnet-core-cli
-
-### Add migration
-
-When addin migration from Automaton.Server project we get this eror message:
-
-Your target project 'Automaton.Studio.Server' doesn't match your migrations assembly 'Automaton.Studio.Server.MySql.Migrations'. Either change your target project or change your migrations assembly.                                       
-Change your migrations assembly by using DbContextOptionsBuilder. E.g. options.UseSqlServer(connection, b => b.MigrationsAssembly("Automaton.Studio.Server")). By default, the migrations assembly is the assembly containing the DbContext. 
-Change your target project to the migrations project by using the Package Manager Console's Default project drop-down list, or by executing "dotnet ef" from the directory containing the migrations project.                                
-
-1. Navigate to Automaton.Studio.Server
-2. Make sure DatabaseType configuration is "MySql"
- ``"AppConfig": {
-    "DatabaseType":  "MySql"
-  }``
-3. Execute in command line:
-``dotnet ef migrations add FlowExecutions --project ../Automaton.Studio.Server.MySql.Migrations/Automaton.Studio.Server.MySql.Migrations.csproj``
-
-Do the same for MsSql migration
-
-1. Navigate to Automaton.Studio.Server
-2. Make sure DatabaseType configuration is "MsSql"
- ``"AppConfig": {
-    "DatabaseType":  "MsSql"
-  }``
-3. Execute in command line:
-``dotnet ef migrations add FlowExecutions --project ../Automaton.Studio.Server.MsSql.Migrations/Automaton.Studio.Server.MsSql.Migrations.csproj``
-
-### Run migration to create/update database
-
-From console
-
-``dotnet ef database update --context ApplicationDbContext --project Automaton.Studio.Server.MySql.Migrations``
-
-From VS Package Manager Console
-
-Make sure default project us Automaton.Studio.Server
-
-Execute ``Update-Database``
-
-It will apply migrations for the provider specified in app.settings:
-
-``
-  "AppConfig": {
-    "DatabaseType":  "MySql"
-  },
-``
-
-Valid entries are:
-
-* MySql
-* MsSql
-
-
 
 ### Swagger URLs
 
